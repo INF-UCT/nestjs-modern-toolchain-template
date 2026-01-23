e2c95d3f1a9464009a7066e324a799579d7804b5
BenjaminEspinozaFK <bespinoza2021@alu.uct.cl>
2026-01-23 01:29:27 -0300

    feat: :art: Se configura el Linting y formato de las pruebas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,18 +3,15 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
-    @Get()
-    getRoot() {
-        return this.appService.getRoot();
-    }
+  @Get()
+  getRoot() {
+    return this.appService.getRoot();
+  }
 
-    @Get('items/:id')
-    getItem(
-        @Param('id') id: string,
-        @Query('q') q?: string,
-    ) {
-        return this.appService.getItem(parseInt(id), q);
-    }
+  @Get('items/:id')
+  getItem(@Param('id') id: string, @Query('q') q?: string) {
+    return this.appService.getItem(parseInt(id), q);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    getRoot() {
-        return { Hello: 'World' };
-    }
+  getRoot() {
+    return { Hello: 'World' };
+  }
 
-    getItem(id: number, q?: string) {
-        return {
-            item_id: id,
-            q: q || null,
-        };
-    }
+  getItem(id: number, q?: string) {
+    return {
+      item_id: id,
+      q: q || null,
+    };
+  }
 }
